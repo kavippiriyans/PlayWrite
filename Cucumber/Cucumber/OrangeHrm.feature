71646f595489c13c7bfb429aa9cf62a626c75392
Kavippiriyan <138883580+Kavippiriyan@users.noreply.github.com>
2023-07-27 17:44:47 +0530

    Cucumber

--- Cucumber/Cucumber/OrangeHrm.feature
+++ Cucumber/Cucumber/OrangeHrm.feature
@@ -1,6 +1,7 @@
 Feature: OrangeHrm Login Page
 
-Background: Given Url(https://https://opensource-demo.orangehrmlive.com/web/index.php/auth/login)
+Background: 
+Given Url: "https://https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
             Then OrangeHrm Login Page is Displayed
 
 Scenario: Valid Username with Valid Password
@@ -11,3 +12,12 @@ Given Password "admin123"
 Then Entered Password is displayed in dot shape
 And click on login button
 Then OrangeHrm Homepage is displayed
+
+Scenario: Invalid Username with Valid Password 
+ 
+Given Username "Admi"
+Then Entered Username is dispalyed
+Given Password "admin123"
+Then Entered Password is displayed in dot shape
+And click on login button 
+Then Error message is displayed like Invalid credentials
--- Cucumber/src/test/java/OrangeHrm/TestCase.java
+++ Cucumber/src/test/java/OrangeHrm/TestCase.java
@@ -7,32 +7,37 @@ import io.cucumber.java.en.Then;
 
 public class TestCase 
 {
+	@Given("Url: {string}")
+	public void url(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    DriverClass.WebDriver();
+	    System.out.println("Enter the OrangHrmLogin Page Url ");
+	}
 
 	@Then("OrangeHrm Login Page is Displayed")
 	public void orange_hrm_login_page_is_displayed() {
 	    // Write code here that turns the phrase above into concrete actions
-		DriverClass.WebDriver();
-	   System.out.println("Enter the OrangeHrm Login pageUrl");
+	   
 	}
 
 	@Given("Username {string}")
 	public void username(String string) {
 	    // Write code here that turns the phrase above into concrete actions
-		Loginpageclass.usernamekeys("Admin");
-	    System.out.println("Enter the valid Username");
+	    System.out.println("Enter the username");
+	    Loginpageclass.usernamekeys(string);
 	}
 
 	@Then("Entered Username is displayed")
 	public void entered_username_is_displayed() {
 	    // Write code here that turns the phrase above into concrete actions
-	    
+	  
 	}
 
 	@Given("Password {string}")
 	public void password(String string) {
 	    // Write code here that turns the phrase above into concrete actions
-		Loginpageclass.passwordkeys("admin123");
-	    System.out.println("Enter the Valid Password");
+	   System.out.println("Enter the password");
+	   Loginpageclass.passwordkeys(string);
 	}
 
 	@Then("Entered Password is displayed in dot shape")
@@ -50,6 +55,18 @@ public class TestCase
 	@Then("OrangeHrm Homepage is displayed")
 	public void orange_hrm_homepage_is_displayed() {
 	    // Write code here that turns the phrase above into concrete actions
-	    
+	   
+	}
+
+	@Then("Entered Username is dispalyed")
+	public void entered_username_is_dispalyed() {
+	    // Write code here that turns the phrase above into concrete actions
+		
+	}
+
+	@Then("Error message is displayed like Invalid credentials")
+	public void error_message_is_displayed_like_invalid_credentials() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
 	}
 }
