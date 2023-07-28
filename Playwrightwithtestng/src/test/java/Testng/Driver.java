e4d44760945c107f95f465a654c800c294c72ed6
Kavippiriyan <138883580+Kavippiriyan@users.noreply.github.com>
2023-07-28 18:30:03 +0530

    d

---
+++ Playwrightwithtestng/src/test/java/Testng/Driver.java
@@ -0,0 +1,14 @@
+package Testng;
+
+import com.microsoft.playwright.Playwright;
+
+public class Driver 
+{
+
+	    
+	
+	public static void driver(String url)
+	{
+		Playwright pw=Playwright.create();
+	}
+}
