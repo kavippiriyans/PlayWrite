775f2d49cd70c6e1cbc12fc307900dc18ac3fe1b
Kavippiriyan <138883580+Kavippiriyan@users.noreply.github.com>
2023-07-29 18:29:12 +0530

    Updated cucumber with playwright

--- PlayWrite/src/test/java/Assert/OrangeHrmValidation.java
+++ PlayWrite/src/test/java/Assert/OrangeHrmValidation.java
@@ -1,75 +1,50 @@
-package Assert;
-
-import org.openqa.selenium.WebElement;
-
-import com.microsoft.playwright.Locator;
-
-public class OrangeHrmValidation extends DriverClass
-{
-
-
-	public static void loginpageValidation(Locator loginpage) throws Exception
-	{
-		if(!(loginpage.isVisible()))
-		{
-			throw new Exception("login page not displyed");
-		}
-		else
-		{
-			System.out.println("login page  is displayed");
-		}
-
-	}
-
-	public static void EnteredUsernameValidation(String Actualusername ) throws Exception
-	{
-		if(!(Actualusername.equals(Loginpageclass.Username_xpath.getAttribute("value"))))
-		{
-			throw new Exception("Entered username is not displyed");
-		}
-		else
-		{
-			System.out.println("Entered username is displayed");
-		}
-
-	}
-
-	public static void EnteredPasswordValidation(String Actualpassword ) throws Exception
-	{
-		if(!(Actualpassword.equals(Loginpageclass.Password_xpath.getAttribute("value"))))
-		{
-			throw new Exception("Entered password is not displyed");
-		}
-		else
-		{
-			System.out.println("Entered password is displayed");
-		}
-
-	}
-
-	public static void HomepageValidation(Locator homepage) throws Exception
-	{
-		if(!(homepage.isVisible()))
-		{
-			throw new Exception("Home page is not dispalyed");
-		}
-		else
-		{
-			System.out.println("Home page is displayed");
-		}
-	}
-	
-	public static void IuserVpassValidation(Locator IuserVpass) throws Exception
-	{
-		if(!(IuserVpass.isVisible()))
-		{
-			throw new Exception("Invalid credentials is not dispalyed");
-		}
-		else
-		{
-			System.out.println("Invalid credentials is displayed");
-		}
-	}
-}
-
+//package Assert;
+//
+//import static org.testng.Assert.assertEquals;
+//import static org.testng.Assert.assertTrue;
+//
+//import org.openqa.selenium.WebElement;
+//
+//
+//import com.microsoft.playwright.Locator;
+//
+//
+//
+//public class OrangeHrmValidation extends DriverClass
+//{
+//
+//	public void openpage()
+//	{
+//		DriverClass.driver("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+//		String acturl="https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+//		assertEquals(acturl, DriverClass.p.url(), "Incorrect url");
+//		System.out.println("Testcase1 pass");
+//	}
+//	
+//	
+//	public void valid_credential()
+//	{
+//		Loginpageclass.usernamekeys("Admin");
+//		String actemail="Admin";
+//		assertEquals(actemail, Loginpageclass.username().inputValue(), "Invalid mail id");
+//		Loginpageclass.passwordkeys("admin123");
+//		String actpass="admin123";
+//		assertEquals(actpass, Loginpageclass.Pass().inputValue(), "Invalid pass");
+//		Loginpageclass.loginbuttonkeys();;
+//		assertTrue(Loginpageclass.Homepage().isVisible(), "Dashboard page open");
+//	}
+//	
+//	public void Invalid_credential()
+//	{
+//		Loginpageclass.NullUser_ValidPass("Admi");
+//		String actemail="Admi";
+//		assertEquals(actemail, Loginpageclass.username(), "Invalid mail id");
+//		Loginpageclass.passwordkeys("admin123");
+//		String actpass="admin123";
+//		assertEquals(actpass, Loginpageclass.Pass().inputValue(), "Invalid pass");
+//		Loginpageclass.loginbuttonkeys();
+//		assertTrue(Loginpageclass.IuserVpass().isVisible(), "Errormsg is Display");
+//	}
+//}
+//
 
